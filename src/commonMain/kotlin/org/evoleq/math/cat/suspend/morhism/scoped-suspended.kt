4eeb8f3246f231f8d209497f7848e19e6420ac49
Florian Schmidt <florian.schmidt@platis.de>
2020-05-13 13:12:43 +0200

    Added modules ~template,~morphism,~state

---
+++ src/commonMain/kotlin/org/evoleq/math/cat/suspend/morhism/scoped-suspended.kt
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2018-2020 Dr. Florian Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evoleq.math.cat.suspend.morhism
+
+import kotlinx.coroutines.CoroutineScope
+import org.evoleq.math.cat.marker.MathCatDsl
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+
+interface ScopedSuspended<in S, out T> : ReadOnlyProperty<Any?, suspend CoroutineScope.(S) -> T> {
+    val function: suspend CoroutineScope.(S)->T
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): suspend CoroutineScope.(S) -> T = { s ->function(s)}
+
+    fun onScope(scope: CoroutineScope): Suspended<S, T> = Suspended{ s: S -> function(scope,s)}
+
+    open suspend operator fun<T1> times(other: ScopedSuspended<T, T1>): ScopedSuspended<S, T1> = ScopedSuspended {
+            s -> other.function( this, function(s) ) }
+}
+
+@Suppress("FunctionName")
+@MathCatDsl
+fun <S, T> ScopedSuspended(function: suspend CoroutineScope.(S)->T): ScopedSuspended<S, T> = object : ScopedSuspended<S, T> {
+    override val function: suspend CoroutineScope.(S) -> T = function
+}
+
+/**
+ * Force delegation by function
+ */
+@MathCatDsl
+fun <S, T> by(arrow: ScopedSuspended<S, T>): suspend CoroutineScope.(S)->T = arrow.function
+
+@MathCatDsl
+fun <S> CoroutineScope.evolve(data: S): Pair<CoroutineScope,S> = Pair(this,data)
+
+@MathCatDsl
+suspend infix fun <S, T> Pair<CoroutineScope,S>.by(arrow: ScopedSuspended<S, T>): T = arrow.function(first,second)
---
+++ src/commonMain/kotlin/org/evoleq/math/cat/suspend/morhism/suspended.kt
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2018-2020 Dr. Florian Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evoleq.math.cat.suspend.morhism
+
+import org.evoleq.math.cat.marker.MathCatDsl
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+interface Suspended<in S, out T> : ReadOnlyProperty<Any?, suspend (S)->T> {
+    val morphism: suspend (S)->T
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): suspend (S) -> T = morphism
+
+    suspend fun<T1> times(other: Suspended<T, T1>): Suspended<S, T1> = Suspended { s -> other.morphism(morphism(s))}
+}
+
+@Suppress("FunctionName")
+@MathCatDsl
+fun <S, T> Suspended(function: suspend (S)->T): Suspended<S, T> = object : Suspended<S, T> {
+    override val morphism: suspend (S) -> T
+        get() = function
+}
+
+
